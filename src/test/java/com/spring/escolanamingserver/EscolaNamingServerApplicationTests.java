90c1627b184b761aac2eed7a416a4b87ab470662
brenno <brennorebeiro@gmail.com>
2024-08-02 00:15:07 -0300

    Add conf eureka

---
+++ src/test/java/com/spring/escolanamingserver/EscolaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.escolanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaNamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
